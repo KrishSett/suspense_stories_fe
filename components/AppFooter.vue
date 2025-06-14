8be5584c038902b66b27dd8bf80cc5db4355508b
KrishSett <krishnendu.sett@capitalnumbers.com>
2025-06-14 10:58:24 +0530

    init setup

---
+++ components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="bg-gray-100 dark:bg-gray-800 py-6">
+        <div class="mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-between items-center">
+                <div class="mb-4 md:mb-0">
+                    <p class="text-gray-600 dark:text-gray-300">
+                        © 2025 My Awesome Site. All rights reserved.
+                    </p>
+                </div>
+                <div class="flex space-x-4">
+                    <a href="#" class="text-gray-600 dark:text-gray-300 hover:text-primary-500">
+                        <UIcon name="i-heroicons-facebook" class="w-5 h-5" />
+                    </a>
+                    <a href="#" class="text-gray-600 dark:text-gray-300 hover:text-primary-500">
+                        <UIcon name="i-heroicons-twitter" class="w-5 h-5" />
+                    </a>
+                    <a href="#" class="text-gray-600 dark:text-gray-300 hover:text-primary-500">
+                        <UIcon name="i-heroicons-instagram" class="w-5 h-5" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
